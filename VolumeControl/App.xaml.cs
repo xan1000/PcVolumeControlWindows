518849ff607b28b82500e30ba4765d4fee7fa108
Matthew Bolger <xan1000@gmail.com>
2020-07-03 17:35:00 +1000

    Fixed regression bug.

--- VolumeControl/App.xaml.cs
+++ VolumeControl/App.xaml.cs
@@ -331,6 +331,7 @@ namespace VolumeControl
                     try
                     {
                         //Console.WriteLine("client message: " + message);
+                        Debug.WriteLine(message);
                         var pcAudio = JsonConvert.DeserializeObject<PcAudio>(message, m_jsonsettings);
 
                         if(pcAudio == null)
@@ -366,7 +367,11 @@ namespace VolumeControl
 
             lock (m_lock)
             {
-                var audioState = new PcAudio { protocolVersion = PROTOCOL_VERSION };
+                var audioState = new PcAudio
+                {
+                    protocolVersion = PROTOCOL_VERSION,
+                    applicationVersion = APPLICATION_VERSION
+                };
 
                 cleanUpSessionKeepers();
 
@@ -375,6 +380,16 @@ namespace VolumeControl
                     return true;
 
                 var defaultDeviceId = defaultDevice.Id.ToString();
+
+                // Add all available audio devices to our list of device IDs
+                var devices = m_coreAudioController.GetPlaybackDevices();
+                foreach (var device in devices)
+                {
+                    if (device.State == DeviceState.Active)
+                    {
+                        audioState.deviceIds.Add(device.Id.ToString(), device.FullName);
+                    }
+                }
 
                 // Master device updates
                 if (audioUpdate?.defaultDevice != null)
@@ -425,11 +440,11 @@ namespace VolumeControl
                 }
 
                 // Create our default audio device and populate it's volume and mute status
-                var audioDevice = new AudioDevice(defaultDeviceId);
+                var audioDevice = new AudioDevice(defaultDevice.FullName, defaultDeviceId);
                 audioState.defaultDevice = audioDevice;
 
                 var defaultPlaybackDevice = m_coreAudioController.DefaultPlaybackDevice;
-                audioDevice.masterVolume = (float)defaultPlaybackDevice.Volume;
+                audioDevice.masterVolume = (float) defaultPlaybackDevice.Volume;
                 audioDevice.masterMuted = defaultPlaybackDevice.IsMuted;
 
                 // Go through all audio sessions
@@ -478,7 +493,7 @@ namespace VolumeControl
                             using var process = Process.GetProcessById(session.ProcessId);
                         }
 
-                        var audioSession = new AudioSession(session.Id, (float)session.Volume, session.IsMuted);
+                        var audioSession = new AudioSession(sessionName, session.Id, (float)session.Volume, session.IsMuted);
                         audioDevice.sessions.Add(audioSession);
                     }
                     catch (Exception)
